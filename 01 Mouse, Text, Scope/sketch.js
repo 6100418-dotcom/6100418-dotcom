042058fca84a3037721e6bb9d87718b85e9fb43f
6100418-dotcom <6100418@spsd.sk.ca>
2026-09-16 14:10:04 -0600

    demo part 2

--- 01 Mouse, Text, Scope/sketch.js
+++ 01 Mouse, Text, Scope/sketch.js
@@ -1,37 +1,61 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Mouse, Text, Scope
+// Mason Wick
+// Sept 15, 2026
 
-//global
-let x = 100;
-let y = 100;
+// Global Variables
+let x = 100;  let y = 100;
+let c = "green";
 
 async function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
-  textSize(40);
+  background(222);
+  mouseReport();
+  updateSquare();
+}
 
+function mouseReport(){
+  textSize(40);    
+
+  //mouse-related system variables (managed by library)
+  fill("black");
   let mouseInfo = mouseIsPressed + " "
                   + mouseButton.left + " "
                   + mouseButton.center + " "
-                  + mouseButton.right + " ";
+                  + mouseButton.right;
   text(mouseInfo, mouseX, mouseY);
-
-  drawSquare;
+  
 }
 
-function frawSquare(){
-  //draws a square
-  square(x, y, 50);
+function updateSquare(){
+  if(keyIsDown(LEFT_ARROW)){
+    x-=5;
+  }
+  if(keyIsDown(RIGHT_ARROW)){
+    x+=5;
+  }
+  if(keyIsDown(UP_ARROW)){
+    y-=5;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    y+=5;
+  }
+
+  //draw our character (a square) on screen
+  fill(c);
+  square(x,y,50);
 }
 
-function keyPressed() {
-  //prints what key is pressed
-  print(key + " " +   keyCode);
+function keyPressed(){
+  //this function calls automatically
+  // - for single press captures
+  print(key + " " + keyCode);
+  // for interest's sake → 
+  // [ALT] - 2 to 4 digit number
+  // – →←↓↑ unicode
 }
+
+
+
